0020bd936590b9cbb261b7ad18514293694b9ab7
Tolulope <lanre.toluwa@gmail.com>
2021-01-15 20:56:19 +0100

    models, serialization, viewsets and routing for api app

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Assignment, GradedAssignment, Question, Choice
+
+admin.site.register(Assignment)
+admin.site.register(GradedAssignment)
+admin.site.register(Question)
+admin.site.register(Choice)
