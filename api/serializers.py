8c0b811cb1fefef17b5f0c9efabadcf23e39aad4
Tolulope <lanre.toluwa@gmail.com>
2021-01-19 20:07:44 +0100

    add related name to Question's Assignment field, serialize question and update AssignmentSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
-from .models import Assignment
+from .models import Assignment, Question
+
+
+class StringSerializer(serializers.StringRelatedField):
+    def to_internal_value(self, value):
+        return value
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = StringSerializer(many=True)
+    answer = StringSerializer()
+
+    class Meta:
+        model = Question
+        fields = ('__all__')
 
 
 class AssignmentSerializer(serializers.ModelSerializer):
+    questions = serializers.SerializerMethodField()
+    teacher = StringSerializer()
+
     class Meta:
         model = Assignment
         fields = ('__all__')
+
+    def get_questions(self, obj):
+        questions = QuestionSerializer(obj.questions.all(), many=True).data
+        return questions
